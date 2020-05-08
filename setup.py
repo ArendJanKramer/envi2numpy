aa4f2461b7d2b5f32f4866273a66ae7bd48728ea
Arend Jan Kramer <aj.website@gmail.com>
2020-05-08 10:16:17 +0200

    Updated installation tools

--- setup.py
+++ setup.py
@@ -1,25 +1,33 @@
+import os
+
+import numpy
 from setuptools import setup
 from setuptools.extension import Extension
 
-import os
-
 module1 = Extension('envi2numpy',
-                    sources = ['src/pythonwrapper.cpp', 'src/INIReader.cpp','src/ini.c','src/cnpy.cpp','src/envi_parser.cpp','src/numpy_helpers.cpp'],
-                    include_dirs = ['src','/home/arendjan/miniconda3/envs/arendjan/lib/python3.6/site-packages/numpy/core/include/'],
-		extra_compile_args=['-std=c++11','-O2'])
+                    sources=['src/pythonwrapper.cpp', 'src/INIReader.cpp', 'src/ini.c', 'src/cnpy.cpp',
+                             'src/envi_parser.cpp', 'src/numpy_helpers.cpp'],
+                    include_dirs=['src',
+                                  numpy.get_include()],
+                    extra_compile_args=['-std=c++11', '-O2'])
 
-setup (name = 'envi2numpy',
-       version = '1.2',
-       description = 'Package used for reading hyperspectral captures',
-       ext_modules = [module1],
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-    url='https://github.com/ArendJanKramer/envi2numpy',
+setup(name='envi2numpy',
+      version='1.3',
+      description='Package used for reading and converting hyperspectral ENVI captures to numpy',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      ext_modules=[module1],
 
-    # Author details
-    author='Arend Jan Kramer',
-    author_email='github@arendjan.eu',
+      url='https://github.com/ArendJanKramer/envi2numpy',
 
-    # Choose your license
-    license='MIT'
-)
+      # Author details
+      author='Arend Jan Kramer',
+      author_email='github@arendjan.eu',
 
+      # Choose your license
+      license='gpl-2.0'
+      )
